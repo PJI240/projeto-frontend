c907608648706be873d8f16d36fff618e305c0c0
policarpofelipe <policarpofelipe@gmail.com>
2025-09-23 10:53:57 -0300

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,7 @@ import { createRoot } from 'react-dom/client'
 
 import { App } from './App.jsx'
 import "./styles/global.css";
-import "./styles/theme.css";
+import "./styles/themes.css";
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
