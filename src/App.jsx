fe09802bff45829273928181df7330801c448e6c
policarpofelipe <policarpofelipe@gmail.com>
2025-09-26 14:28:27 -0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Login from './pages/Login';
 import Register from './pages/Register';
-import Dashboard from './pages/Dashboard'; // ← Adicione esta linha
+import Dashboard from './pages/Dashboard'; 
 
 function App() {
   return (
