f52f4c0eefaca2a855e26403cc2265516d61f340
policarpofelipe <policarpofelipe@gmail.com>
2025-09-26 14:23:23 -0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,18 @@
-import { Routes, Route, Navigate } from "react-router-dom";
-import Login from "./pages/Login.jsx";
-import Register from "./pages/Register.jsx";
-import Dashboard from "./pages/Dashboard.jsx";
+// src/App.jsx
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Login from './pages/Login';
+import Register from './pages/Register';
 
-export default function App() {
+function App() {
   return (
-    <Routes>
-      <Route path="/login" element={<Login />} />
-      <Route path="/dashboard" element={<Dashboard />} />
-      <Route path="/" element={<Navigate to="/dashboard" replace />} />
-      
-    </Routes>
+    <Router>  
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+      </Routes>
+    </Router>
   );
 }
+
+export default App;
