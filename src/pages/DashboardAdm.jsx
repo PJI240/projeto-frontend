2c71ca779b6a95b2b1efc3c0a7197b717f40c05b
policarpofelipe <policarpofelipe@gmail.com>
2025-09-27 22:26:37 -0300

    Criar DashboardAdm.jsx

---
+++ src/pages/DashboardAdm.jsx
@@ -0,0 +1,12 @@
+export default function DashboardAdm() {
+  return (
+    <>
+      <header className="main-header">
+        <div className="header-content">
+          <h1>Painel do Administrador</h1>
+          <p>Em construção…</p>
+        </div>
+      </header>
+    </>
+  );
+}
