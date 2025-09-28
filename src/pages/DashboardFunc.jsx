f8a9cd40055ada86fa88bca911f18e180acffa31
policarpofelipe <policarpofelipe@gmail.com>
2025-09-27 22:30:37 -0300

    Criar DashboardFunc.jsx

---
+++ src/pages/DashboardFunc.jsx
@@ -0,0 +1,14 @@
+// src/pages/DashboardFunc.jsx
+export default function DashboardFunc() {
+  return (
+    <>
+      <header className="main-header">
+        <div className="header-content">
+          <h1>Meu Painel</h1>
+          <p>Visualize sua escala e registre ponto.</p>
+        </div>
+      </header>
+      <section><p>Em construção…</p></section>
+    </>
+  );
+}
