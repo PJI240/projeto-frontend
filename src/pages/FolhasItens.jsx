936668a5f0776ceb3966b9fa618303c76dfa95f9
policarpofelipe <policarpofelipe@gmail.com>
2025-09-27 22:38:44 -0300

    Criar FolhasItens.jsx

---
+++ src/pages/FolhasItens.jsx
@@ -0,0 +1,14 @@
+// src/pages/FolhasItens.jsx
+export default function FolhasItens() {
+  return (
+    <>
+      <header className="main-header">
+        <div className="header-content">
+          <h1>Itens de Folha</h1>
+          <p>Proventos, descontos, horas extras, etc.</p>
+        </div>
+      </header>
+      <section><p>Lista / CRUD de itens…</p></section>
+    </>
+  );
+}
