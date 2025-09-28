2467bf09f4483179e23f2ca35bf3a9d603a556e5
policarpofelipe <policarpofelipe@gmail.com>
2025-09-27 22:31:43 -0300

    Criar Pessoas.jsx

---
+++ src/pages/Pessoas.jsx
@@ -0,0 +1,14 @@
+// src/pages/Pessoas.jsx
+export default function Pessoas() {
+  return (
+    <>
+      <header className="main-header">
+        <div className="header-content">
+          <h1>Pessoas</h1>
+          <p>Cadastro de pessoas físicas vinculadas.</p>
+        </div>
+      </header>
+      <section><p>Lista / CRUD de pessoas…</p></section>
+    </>
+  );
+}
