06a0736024bbae00a9146cc6ee7238981e57469b
policarpofelipe <policarpofelipe@gmail.com>
2025-09-27 22:36:54 -0300

    Criar Ocorrencias.jsx

---
+++ src/pages/Ocorrencias.jsx
@@ -0,0 +1,14 @@
+// src/pages/Ocorrencias.jsx
+export default function Ocorrencias() {
+  return (
+    <>
+      <header className="main-header">
+        <div className="header-content">
+          <h1>Ocorrências</h1>
+          <p>Feriados, atestados, folgas e outros.</p>
+        </div>
+      </header>
+      <section><p>Lista / CRUD de ocorrências…</p></section>
+    </>
+  );
+}
