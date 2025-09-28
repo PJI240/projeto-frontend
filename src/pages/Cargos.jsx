95f1f9505390ae7a49e9d1569828db5df8ce7f2a
policarpofelipe <policarpofelipe@gmail.com>
2025-09-27 22:33:26 -0300

    Criar Cargos.jsx

---
+++ src/pages/Cargos.jsx
@@ -0,0 +1,14 @@
+// src/pages/Cargos.jsx
+export default function Cargos() {
+  return (
+    <>
+      <header className="main-header">
+        <div className="header-content">
+          <h1>Cargos</h1>
+          <p>Funções/cargos disponíveis na empresa.</p>
+        </div>
+      </header>
+      <section><p>Lista / CRUD de cargos…</p></section>
+    </>
+  );
+}
