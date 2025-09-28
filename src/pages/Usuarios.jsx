232d2c2d998dcd3790759f5200156eb0fd16305f
policarpofelipe <policarpofelipe@gmail.com>
2025-09-27 22:31:16 -0300

    Criar Usuarios.jsx

---
+++ src/pages/Usuarios.jsx
@@ -0,0 +1,14 @@
+// src/pages/Usuarios.jsx
+export default function Usuarios() {
+  return (
+    <>
+      <header className="main-header">
+        <div className="header-content">
+          <h1>Usuários</h1>
+          <p>Gerencie contas de acesso ao sistema.</p>
+        </div>
+      </header>
+      <section><p>Lista / CRUD de usuários…</p></section>
+    </>
+  );
+}
