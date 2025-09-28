b9749407ff98d41ac49da4cb9e6b318647025df0
policarpofelipe <policarpofelipe@gmail.com>
2025-09-27 22:36:27 -0300

    Criar Apontamentos.jsx

---
+++ src/pages/Apontamentos.jsx
@@ -0,0 +1,14 @@
+// src/pages/Apontamentos.jsx
+export default function Apontamentos() {
+  return (
+    <>
+      <header className="main-header">
+        <div className="header-content">
+          <h1>Apontamentos</h1>
+          <p>Registros de entrada/saída e ajustes.</p>
+        </div>
+      </header>
+      <section><p>Lista / CRUD de apontamentos…</p></section>
+    </>
+  );
+}
