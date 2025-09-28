b033e2747ccf64a238348b49e5748c0e2f1f28bc
policarpofelipe <policarpofelipe@gmail.com>
2025-09-27 22:33:55 -0300

    Criar Funcionarios.jsx

---
+++ src/pages/Funcionarios.jsx
@@ -0,0 +1,14 @@
+// src/pages/Funcionarios.jsx
+export default function Funcionarios() {
+  return (
+    <>
+      <header className="main-header">
+        <div className="header-content">
+          <h1>Funcionários</h1>
+          <p>Vínculo de pessoas a cargos e empresa.</p>
+        </div>
+      </header>
+      <section><p>Lista / CRUD de funcionários…</p></section>
+    </>
+  );
+}
