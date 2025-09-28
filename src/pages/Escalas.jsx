f9e56b5e6d727e95a1ba4a65c93217088c12535e
policarpofelipe <policarpofelipe@gmail.com>
2025-09-27 22:36:07 -0300

    Criar Escalas.jsx

---
+++ src/pages/Escalas.jsx
@@ -0,0 +1,14 @@
+// src/pages/Escalas.jsx
+export default function Escalas() {
+  return (
+    <>
+      <header className="main-header">
+        <div className="header-content">
+          <h1>Escalas</h1>
+          <p>Defina turnos e horários.</p>
+        </div>
+      </header>
+      <section><p>Grade de escalas / CRUD…</p></section>
+    </>
+  );
+}
