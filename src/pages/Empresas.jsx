fca19c950d57a16f29e0bab3869a61ba8f9bdeec
policarpofelipe <policarpofelipe@gmail.com>
2025-09-27 22:32:09 -0300

    Criar Empresas.jsx

---
+++ src/pages/Empresas.jsx
@@ -0,0 +1,14 @@
+// src/pages/Empresas.jsx
+export default function Empresas() {
+  return (
+    <>
+      <header className="main-header">
+        <div className="header-content">
+          <h1>Empresas</h1>
+          <p>Cadastro e consulta de empresas.</p>
+        </div>
+      </header>
+      <section><p>Lista / CRUD de empresas…</p></section>
+    </>
+  );
+}
